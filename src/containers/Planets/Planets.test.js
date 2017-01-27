7698a96e0df0ceca3e9ee2cc614384024eb0e769
Abdella <abdellaalioncan@gmail.com>
2017-01-26 21:24:31 -0500

    better example for sinon

--- src/containers/Planets/Planets.test.js
+++ src/containers/Planets/Planets.test.js
@@ -2,17 +2,38 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import Planets from './Planets';
+import sinon from 'sinon';
 
-
-xdescribe('<Planets />', () => {
+describe('<Planets />', () => {
 
 
   it('should fire getPlanets when that button is clicked', () => {
     const wrapper = shallow(<Planets />);
+
+    const spy = sinon.spy(wrapper.instance(), "getPlanets");
+    wrapper.setState({nextUrl: 'foo'});
+    wrapper.update(); //important for the spy to properly work
+
+
     wrapper.find('button').simulate('click');
 
+    expect(spy.calledOnce).to.equal(true);
 
-  })
+  });
+
+  it('should fire getPlanets with the current nextUrl', () => {
+
+    const wrapper = shallow(<Planets />);
+
+    const spy = sinon.spy(wrapper.instance(), "getPlanets");
+    wrapper.setState({nextUrl: 'foo'});
+    wrapper.update(); //important for the spy to properly work
+
+
+    wrapper.find('button').simulate('click');
+
+    expect(spy.calledWith('foo')).to.equal(true);
+  });
 
 
 });
